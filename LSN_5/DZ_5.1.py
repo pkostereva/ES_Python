a313b4f31de09d940959fec2c16d1919a3538cf9
Polina Kostereva <pkostereva@gmail.com>
2018-11-25 19:13:15 +0300

    Update DZ_5.1.py
    Исправила синтаксис

--- LSN_5/DZ_5.1.py
+++ LSN_5/DZ_5.1.py
@@ -1,18 +1,14 @@
 import random
-st = ['самовар', 'весна', 'лето', 'парк', 'мороженное', 'запрос'] #рандомная строка
-r_word = random.choice(st) #выбрали случайное слово
-word_str = list(str(r_word)) #переделали слово в список
+st = ['самовар', 'весна', 'лето', 'парк', 'мороженное', 'запрос']
+r_word = random.choice(st)
+word_str = list(str(r_word))
 a = random.choice(word_str)
-i = word_str.index(a) #индекс рандомной буквы
-word_str[i]='?' #замена на ?
+i = word_str.index(a)
+word_str[i]='?'
 f_user =''.join(word_str)
-#print('Выбираем случайное слово:', r_word)
-#print('Слово - строка:', word_str)
-#print('Индекс:', i)
-#print(':', word_str)
-print(f'Попробуйте угадать букву в слове {f_user)}')
+print(f'Попробуйте угадать букву в слове {f_user}')
 n = str(input('Введите букву под знаком вопроса:'))
 if n == a:
-    print(f'Победа! \nСлово: {r_word)}')
+    print(f'Победа! \nСлово: {r_word}')
 else:
-    print(f'Увы! Попробуйте в другой раз. \nСлово: {r_word)}')
+    print(f'Увы! Попробуйте в другой раз. \nСлово: {r_word}')
