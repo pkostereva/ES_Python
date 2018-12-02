e54b6209a36eab1efad645f0944f6855694f35f9
Polina Kostereva <pkostereva@gmail.com>
2018-12-03 00:12:32 +0300

    Create DZ_6.10.py

---
+++ LSN_6/DZ_6.10.py
@@ -0,0 +1,20 @@
+print('''Простой todo: 
+    1. Добавить задачу.
+    2. Вывести список задач.
+    3. Выход.''')
+task = []
+while True:
+    n = int(input('Укажите число: '))
+    if n == 1:
+        zad = str(input('Введите задачу: '))
+        kat = str(input('Введите категорию к задаче: '))
+        time = str(input('Добавьте время к задаче: '))
+        task.append([zad, kat, time])
+    elif n == 2:
+        for i in range(len(task)):
+            print(f'Задача: {task[i][0]} \nКатегория: {task[i][1]} \nВремя: {task[i][2]}')
+    elif n == 3:
+        break
+    
+    
+    
