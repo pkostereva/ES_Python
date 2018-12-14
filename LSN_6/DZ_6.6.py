a8388a535cd46ac8e75a2aa6aa425aa82498a561
Polina Kostereva <pkostereva@gmail.com>
2018-12-14 18:19:15 +0300

    исправление ошибок

--- LSN_6/DZ_6.6.py
+++ LSN_6/DZ_6.6.py
@@ -1,9 +1,9 @@
 a = str(input('Введите число: '))
 b = list(a)
 s = 0
+print(b)
 for i in range(len(b)): 
-    if i % 2 == 0:
-        continue
-    s = s + pow(int(b[i]), 2)  
+    if int(i) % 2 == 0:
+        s += pow(int(b[i]), 2)
 else:
     print(s)
