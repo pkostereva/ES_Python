6dff1607065685ef268d6467c8ef4433b96fe2de
Polina Kostereva <pkostereva@gmail.com>
2018-12-15 10:31:52 +0300

    исправлены ошибки

--- LSN_9/DZ_9.2.py
+++ LSN_9/DZ_9.2.py
@@ -1,10 +1,12 @@
 from pprint import pprint
 import json
+'''
+1. Добавить задачу.
+2. Вывести список задач.
+3. Выход.
+'''
 n=0
-z=0
-task=dict()
 while True:
-    print('\n1. Добавить задачу.\n2. Вывести список задач.\n3. Выход.')
     n=input('Выберете пункт: ')
     if n.isnumeric():
         if int(n) == 1:
@@ -12,15 +14,16 @@ while True:
             a=input('Задача: ')
             b=input('Категория: ')
             c=input('Время: ')
-            task[z]={0 : a, 1 : b, 2 : c}  
-            with open('task.txt', 'a') as file:
-                print(task[z])
-                file.write( json.dumps(task[z], ensure_ascii=False))
-            z += 1
+            with open('task.json', 'a') as file:
+                file.write(f'Задача: {a} Категория:{b} Время: {c}'+'\n')
+                file.close()
         elif int(n) == 2:
             print('\n')
-            for i in range(len(task)):
-                print(f'Задача: {task[i][0]}, Категория: {task[i][1]}, Время: {task[i][2]}')
+            with open('task.json', 'r') as file:
+                print(file.read())
+                file.close()
+            #for i in range(len(task)):
+            #    print(f'Задача: {task[i][0]}, Категория: {task[i][1]}, Время: {task[i][2]}')
         elif int(n) == 3:
             break
     else:
